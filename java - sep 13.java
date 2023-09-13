503030e1ac84ba850e0d7fd06e9a06a7f4c8dd54
rajveersgithub <144941646+rajveersgithub@users.noreply.github.com>
2023-09-13 14:00:13 -0400

    Add files via upload

---
+++ java - sep 13.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    int x;//declaration
+    x=12;//initilization
+      int y;y=4;
+     System.out.println(x + y);
+    System.out.println(x - y);
+    System.out.println(x * y);
+    System.out.println(x / y);
+  }
+}
