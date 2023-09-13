f38fc6b66d5a6bae6b8dc173b7cb7eba59bc0b1c
rajveersgithub <144941646+rajveersgithub@users.noreply.github.com>
2023-09-13 13:00:01 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.print("T");
+    System.out.print("r");
+    System.out.print("i");
+    System.out.print("a");
+    System.out.print("n");
+    System.out.print("g");
+    System.out.print("l");
+    System.out.print("e");
+  }
+}
