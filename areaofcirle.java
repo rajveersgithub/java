e94dc828d91b36b9a14bf79c594d2c7979dfa950
rajveersgithub <144941646+rajveersgithub@users.noreply.github.com>
2023-09-15 13:57:09 -0400

    Create areaofcirle.java

---
+++ areaofcirle.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+ double radius=6.7;
+    double area;
+    area= (3.14159*radius*radius);
+      System.out.println("Area of circle is   ", area);
+
+    double radius1=5.9;
+    double area1= (PI*radius1*radius1);
+    System.out.println("Area of circle is   ", area1);
+  }
+}
