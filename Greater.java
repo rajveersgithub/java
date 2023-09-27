4cfa3f6c5bc8ad7c286c7ea54b1993c6bf888f06
rajveersgithub <144941646+rajveersgithub@users.noreply.github.com>
2023-09-27 13:03:38 -0400

    Add files via upload

---
+++ Greater.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    int x=10; int y=20;
+    if(x>y)
+    { //true statment 
+      System.out.println("x is greater");
+    } 
+else //else block
+    { // false statement 
+  System.out.println("y is greater");
+    }
+  }
+  }
