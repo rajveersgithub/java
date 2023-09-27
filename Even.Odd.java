dd2de1380e27afd32ff3f181c11de20ca9bae94a
rajveersgithub <144941646+rajveersgithub@users.noreply.github.com>
2023-09-27 13:14:26 -0400

    Add files via upload

---
+++ Even.Odd.java
@@ -0,0 +1,25 @@
+//Program to find the largest of twi numberl
+class Main {
+  public static void main(String[] args) {
+   /* int x=10; int y=20;
+    if(x>y) //if block
+    { //true satement
+      System.out.println("x is greater");
+      
+    }
+    else //else block
+    { //false statement
+      System.out.println("y is greater");
+      
+    } */
+    //Program is find wheter a number is even or odd
+      int num=10;
+        if (num%2==0)
+  {
+   System.out.println("EVEN NUMBER");
+  }
+  else
+  {
+   System.out.println(num + "IS AN ODD NUMBER");
+  }
+ 
