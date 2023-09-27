7d2c2367d366016e5b81c392eeec162fea682866
rajveersgithub <144941646+rajveersgithub@users.noreply.github.com>
2023-09-27 13:40:50 -0400

    Add files via upload

---
+++ multiple_if_statements.java
@@ -0,0 +1,40 @@
+//Program to find the largest of two number
+class Main {
+  public static void main(String[] args) {
+    /*int x=10; int y=20;
+    if(x>y) //if block
+    { //true satement
+      System.out.println("x is greater");
+      
+    }
+    else //else block
+    { //false statement
+      System.out.println("x is less");
+      
+    }*/
+    int num =10;
+
+      if(num%2==0)
+    {
+      System.out.println( num +" is an Even number");
+    
+    }
+    else
+    {
+      System.out.println( num +" is an Odd number");
+    } 
+int s=25;
+if (s>30)
+{
+  System.out.println("Number is greater than 30");
+}
+  if (s>20)
+  {
+    System.out.println("Number is greater than 20");
+  }
+    if(s>10)
+    {
+      System.out.println("The number is greater than 10");
+    }
+  }
+}
