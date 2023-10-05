b21bc808f94b120ebacdd8919e92ba481837ceed
rajveersgithub <144941646+rajveersgithub@users.noreply.github.com>
2023-10-05 13:13:07 -0400

    Add files via upload

---
+++ Grade.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    int x=80;
+    if (x>90){
+System.out.println("grade is A");
+    }
+  else if (x>80) {
+  System.out.println("grade is B");
+  }
+    else if (x>70) {
+  System.out.println("grade is C");
+    }
+else {
+      System.out.println("Percentage is less than 70");
+}
+  }
+}
